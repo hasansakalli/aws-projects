811df35e44a6d1b3b28c4bbee8664b220f4603f0
HASAN SAKALLI <89208116+hasansakalli@users.noreply.github.com>
2024-03-14 11:16:49 +0100

    ilk portfolio

---
+++ flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    return render_template('index.html', number1=45, number2=50)
+
+
+@app.route('/mult')
+def number():
+     x = 56
+     y = 32  
+     return render_template('body.html', value1=x, value2=y, sum=x*y)
+
+
+if __name__ == "__main__":
+#     app.run(debug=True, port=30000)
+
+     app.run(host= '0.0.0.0/0', port=80)
